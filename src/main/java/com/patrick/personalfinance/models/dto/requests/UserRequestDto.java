b6d9a5f3c2948c00b4cd12cc4e22d6e38422241a
Patrick Batista <patrickbpds@gmail.com>
2025-08-05 15:46:56 -0300

    feat: add UserRequestDto for user registration with validation

---
+++ src/main/java/com/patrick/personalfinance/models/dto/requests/UserRequestDto.java
@@ -0,0 +1,28 @@
+package com.patrick.personalfinance.models.dto.requests;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.With;
+
+@With
+public record UserRequestDto (
+
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 3, max = 80, message = "Name must be between 3 and 80 characters")
+    String name,
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email")
+    @Size(min = 6, max = 80, message = "Email must be between 6 and 80 characters")
+    String email,
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+    String password,
+
+    String photo
+
+){
+
+}
