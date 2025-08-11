d5866acdb706cf21f66e5e18ee8f6a2d8dcc8522
Patrick Batista <patrickbpds@gmail.com>
2025-08-11 10:02:03 -0300

    fix: update userRepository to return optional<user> for findByEmail method

--- src/main/java/com/patrick/personalfinance/repository/UserRepository.java
+++ src/main/java/com/patrick/personalfinance/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.patrick.personalfinance.repository;
 
-import org.apache.catalina.User;
+import com.patrick.personalfinance.models.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    List<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
