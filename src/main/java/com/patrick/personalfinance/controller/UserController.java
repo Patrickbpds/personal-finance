6e18954c6b313fb78912bdad33d283002ef58cf7
Patrick Batista <patrickbpds@gmail.com>
2025-08-15 16:04:21 -0300

    feat: add getAll and getById user endpoints

---
+++ src/main/java/com/patrick/personalfinance/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.patrick.personalfinance.controller;
+
+import com.patrick.personalfinance.models.dto.responses.UserResponseDto;
+import com.patrick.personalfinance.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getAll() {
+        return ResponseEntity.ok(userService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+
+}
