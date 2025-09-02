e17470b2bc6dd43a2196124e4c6a7dadf2838a72
Patrick Batista <patrickbpds@gmail.com>
2025-09-02 19:44:49 -0300

    feat: add JpaUserDetailsService to load user details by email

---
+++ src/main/java/com/patrick/personalfinance/security/service/JpaUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.patrick.personalfinance.security.service;
+
+import com.patrick.personalfinance.models.entity.User;
+import com.patrick.personalfinance.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repo.findByEmail(email)
+                .filter(User::isActive)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found or inactive: " + email));
+    }
+}
