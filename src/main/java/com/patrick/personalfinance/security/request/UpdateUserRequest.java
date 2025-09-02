583727747226cd715e7028f28803c6ffc3b2f22e
Patrick Batista <patrickbpds@gmail.com>
2025-09-02 19:44:17 -0300

    feat: add UpdateUserRequest class for user details update

---
+++ src/main/java/com/patrick/personalfinance/security/request/UpdateUserRequest.java
@@ -0,0 +1,21 @@
+package com.patrick.personalfinance.security.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateUserRequest(
+        @NotBlank(message = "Name cannot be empty")
+        @Size(min = 3, max = 80, message = "Name must be between 3 and 80 characters")
+        String name,
+
+        @NotBlank(message = "Email cannot be empty")
+        @Email(message = "Invalid email")
+        String email,
+
+        @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+        String password,
+
+        String photo
+) {
+}
