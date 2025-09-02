1f375dd3269e5d0de5960b02b1aea964670b9f4f
Patrick Batista <patrickbpds@gmail.com>
2025-09-02 19:42:16 -0300

    ✨ feat: add TokenResponse class for handling JWT token responses

---
+++ src/main/java/com/patrick/personalfinance/security/response/TokenResponse.java
@@ -0,0 +1,20 @@
+package com.patrick.personalfinance.security.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class TokenResponse {
+    @JsonProperty("access_token")
+    private final String token;
+
+    @JsonProperty("token_type")
+    private final String tokenType = "Bearer";
+
+    @JsonProperty("expires_in")
+    private final long expiresIn = 86400;
+
+    public TokenResponse(String token) {
+        this.token = token;
+    }
+}
