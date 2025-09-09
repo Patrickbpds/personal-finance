6a9af211a3633e82a3379b8cf14d7af34521a240
Patrick Batista <patrickbpds@gmail.com>
2025-09-09 19:09:37 -0300

    feat: add CenterCostRequestDto for CenterCost entity representation

---
+++ src/main/java/com/patrick/personalfinance/application/dto/requests/CenterCostRequestDto.java
@@ -0,0 +1,19 @@
+package com.patrick.personalfinance.application.dto.requests;
+
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.UUID;
+
+public record CenterCostRequestDto(
+
+        UUID id,
+
+        @NotBlank(message = "Description cannot be empty")
+        @Size(min = 3, max = 100, message = "Description must be between 3 and 100 characters")
+        String description,
+
+        @Size(max = 500, message = "Observation cannot exceed 500 characters")
+        String observation
+) {
+}
